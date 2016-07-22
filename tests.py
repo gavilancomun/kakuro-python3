f2c5de4753c14f6df07951de27e550cfd0acd60c
James Petry <gavilancomun@gmail.com>
2016-07-22 13:37:42 +0100

    test

--- tests.py
+++ tests.py
@@ -5,13 +5,6 @@ from kakuro import *
 
 class TestStringMethods(unittest.TestCase):
 
-  def test_upper(self):
-      self.assertEqual('foo'.upper(), 'FOO')
-
-  def test_isupper(self):
-      self.assertTrue('FOO'.isupper())
-      self.assertFalse('Foo'.isupper())
-
   def test_drawrow(self):
     line = [da(3, 4), v(), v(1, 2), d(4), e(), a(5), v(4), v(1)]
     result = drawRow(line)
